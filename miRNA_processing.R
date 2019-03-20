bbda59bce2111a7c66f9e75fd5ceee3426f9849f
JackLMc <jacklukemcmurray@gmail.com>
2019-03-20 19:35:13 +0000

    Investigate microRNAs

---
+++ miRNA_processing.R
@@ -0,0 +1,71 @@
+library(UsefulFunctions)
+library(tidyverse)
+library(ggpubr)
+
+my_comparisons <- list(c("MSS-hiCIRC", "MSI-H"),
+                       c("MSS-hiCIRC", "MSS"),
+                       c("MSI-H", "MSS"))
+cbcols <- c("MSS-hiCIRC" = "#999999",
+            "MSI-H" = "#56B4E9",
+            "MSS" = "#009E73")
+
+miRNA <- read.csv("~/Downloads/pancanMiRs_EBadjOnProtocolPlatformWithoutRepsWithUnCorrectMiRs_08_04_16.csv")
+
+head(miRNA)
+
+miRNA1 <-miRNA %>% gather(contains("TCGA"), key = "Patient.ID", value = "FPKM")
+
+head(miRNA1)
+
+colnames(miRNA1)[colnames(miRNA1) == "Patient.ID"] <- "Sample.ID"
+
+miRNA1$Patient.ID <- samptopat(miRNA1$Sample.ID)
+miRNA1$Patient.ID <- gsub("-", ".", miRNA1$Patient.ID)
+
+
+
+mir_Int <- read.csv("./Exploratory_Data/miRNA_interactions.csv")
+CIITA <- droplevels(subset(mir_Int, Target.Gene == "CIITA"))
+
+try <- miRNA1[miRNA1$Gene %in% CIITA$miRNA,]
+
+head(try)
+
+pat_sub <- read.csv("Output/Patient_Subtypes.csv")
+
+these <- merge(pat_sub, try[, c("Patient.ID", "Genes", "FPKM")]) %>% droplevels()
+
+head(these)
+levels(these$Genes)
+
+for(i in levels(these$Genes)){
+  print(i)
+  work <- droplevels(subset(these, Genes == i))
+  work$Rank <- rank(work$FPKM)
+  temp_plot <- ggplot(work, aes(x = Subtype, y = Rank)) +
+    geom_boxplot(alpha = 0.5, width = 0.2) + 
+    geom_violin(aes(Subtype, fill = Subtype),
+                scale = "width", alpha = 0.8) +
+    scale_fill_manual(values = cbcols) +
+    labs(x = "MSI Status", y = paste("Rank transformed", i, "FPKM", sep = " ")) +
+    theme_bw() +
+    theme(axis.text = element_text(size = 16)) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+    theme(legend.direction = "horizontal", legend.position = "top") + 
+    stat_compare_means(comparisons = my_comparisons,
+                       label = "p.signif", method = "wilcox.test")
+  filen <- paste0(i, ".pdf")
+  ggplot2:: ggsave(filen, plot = temp_plot, device = "pdf",
+                   path = "./Figures/miRNAs",
+                   height = 6, width = 6)
+}
+
+head(mir_Int)
+genesofI <- droplevels(subset(mir_Int, miRNA == "hsa-miR-335-5p"& Support.Type != "Functional MTI (Weak)"))
+
+
+levels(genesofI$Target.Gene)
+
+
+droplevels(subset(mir_Int, Target.Gene == "HLA-DRB5" ))
+           
