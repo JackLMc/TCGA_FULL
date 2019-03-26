f0180c95f6e91bd137599ad7a9149d07acb4dbb5
JackLMc <jacklukemcmurray@gmail.com>
2019-03-26 09:31:23 +0000

    Investigating patient weight

--- Clinical.R
+++ Clinical.R
@@ -87,16 +87,152 @@ ggplot(try1, aes(x = Subtype, y = height)) +
 
 
 
-clinical_foll <- read.delim("./Data/clinical.cart.2019-03-15/clinical.tsv")
-clinical_foll$Patient.ID <- gsub("-", ".", clinical_foll$submitter_id)
+clinical_foll <- read.delim("~/Downloads/coadread_tcga_pan_can_atlas_2018_clinical_data.tsv")
+head(clinical_foll)
+
+clinical_foll$Patient.ID <- gsub("-", ".", clinical_foll$Patient.ID)
 
 
 this <- merge(clinical_foll, pat_sub, by = "Patient.ID")
+this <- this[!is.na(this$Patient.Weight), ]
 
-this1 <- droplevels(subset(this, age_at_diagnosis != "--"))
-this1$age_at_diagnosis <- as.numeric(as.character(this1$age_at_diagnosis))
+head(this)
+this$Patient.Weight <- as.numeric(as.character(this$Patient.Weight))
 
-ggplot(this1, aes(x = Subtype, y = age_at_diagnosis)) +
+ggplot(this, aes(x = Subtype.y, y = Patient.Weight)) +
+  geom_boxplot(alpha = 0.5, width = 0.2) + 
+  geom_violin(aes(Subtype.y, fill = Subtype.y),
+              scale = "width", alpha = 0.8) +
+  scale_fill_manual(values = cbcols) +
+  labs(x = "MSI Status", y = "weight") +
+  theme_bw() +
+  theme(axis.text = element_text(size = 16)) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(legend.direction = "horizontal", legend.position = "top") + 
+  stat_compare_means(comparisons = my_comparisons,
+                     label = "p.signif", method = "wilcox.test")
+
+
+
+dcast(this, Subtype.y ~ Radiation.Therapy, length)
+dcast(this, Subtype.y ~ X9q.Status, length)
+colnames(this)
+
+## Survival stats
+Paper_clinical <- read.csv("./Data/Clinical_data/Paper_Clinical.csv")
+Paper_clinical$Patient.ID <- samptopat(Paper_clinical$TCGA.ID)
+Paper_clinical$Patient.ID <- gsub("-", ".", Paper_clinical$Patient.ID)
+
+pat_sub <- read.csv("./Output/Patient_Subtypes.csv")
+
+
+survival <- merge(Paper_clinical, pat_sub, by = "Patient.ID")
+
+surviv1 <- survival[!is.na(survival$Overall.survival..days.), ] %>% droplevels()
+surviv1 <- surviv1[, c("Patient.ID", "Overall.survival..days.", "Survival.event..1.death.", "Microsatellite.instability")]
+surviv1$OS_MONTHS <- surviv1$Overall.survival..days./30.42
+surviv1$OS_STATUS <- ifelse((surviv1$Survival.event..1.death. == 1), "DECEASED", "LIVING")
+surviv1 <- surviv1[, c("Patient.ID", "OS_MONTHS", "OS_STATUS", "Microsatellite.instability")]
+colnames(surviv1)[colnames(surviv1) == "Microsatellite.instability"] <- "MSI_STATUS"
+
+tcga_pub <- tcga_pub_clinical[!is.na(tcga_pub_clinical$OS_MONTHS), ] %>% droplevels()
+tcga_pub <- tcga_pub[, c("Patient.ID", "OS_STATUS", "OS_MONTHS", "MSI_STATUS")]
+tcga_pub <- tcga_pub[!'%in%'(tcga_pub$Patient.ID, surviv1$Patient.ID),]
+
+att <- rbind(tcga_pub, surviv1)
+att$MSI_STATUS <- ifelse((att$MSI_STATUS == "MSI-H"), "MSI-H", "MSS")
+clin6 <- att
+clin6 <- clin6[!duplicated(clin6),]
+
+
+clin6$died <- ifelse((clin6$OS_STATUS == "LIVING"), F, T)
+clin6$OS_YEARS <- clin6$OS_MONTHS / 12
+clin7 <- merge(clin6, pat_sub, by = "Patient.ID")
+
+library(survival)
+clin7 <- clin7[!duplicated(clin7), ]
+os.surv <- Surv(clin7$OS_MONTHS, clin7$died)
+
+
+fit1 <- survfit(os.surv ~ MSI_STATUS, data = clin7)
+library(survminer)
+
+survp <- ggsurvplot(fit1, pval = T, pvalmethod = T, palette = c("#56B4E9",  "#009E73", "#999999"), 
+                    risk.table = F)
+ggsave("./Figures/Clinical/survival_comb.pdf", plot = survp$plot, 
+       height = 6, width = 6)
+
+library(reshape2)
+dcast(clin7, Subtype ~ OS_STATUS, length)
+
+
+# pat_sub$Patient.ID[!'%in%'(pat_sub$Patient.ID, att$Patient.ID)] %>% droplevels()
+
+
+head(survival)
+vir <- survival
+vir$Viral <- ifelse((is.na(vir$Virus.detection.)), "NC", as.character(vir$Virus.detection.))
+vir$Viral <- as.factor(vir$Viral)
+
+dcast(Subtype ~ Viral, vir)
+
+dcast(vir, Subtype ~ Viral, length)
+
+head(vir)
+
+# PANCAN stuff
+this <- read.delim("~/Downloads/clinical_PANCAN_patient_with_followup.tsv")
+this$Patient.ID <- gsub("-", ".", this$bcr_patient_barcode)
+
+pat_sub <- read.csv("./Output/Patient_Subtypes.csv")
+that <- merge(this, pat_sub, by = "Patient.ID")
+
+emptycols <- sapply(that, function (k) all(is.na(k)))
+that1 <- that[!emptycols]
+
+head(that1)
+
+View(colnames(that1))
+
+str(that1$weight)
+
+that2 <- that1[!is.na(that1$weight),]
+that2 <- droplevels(subset(that2, weight != "[Not Available]"))
+that2 <- droplevels(subset(that2, weight != "."))
+levels(that2$weight)
+
+that2$weight <- as.numeric(as.character(that2$weight))
+
+ggplot(that2, aes(x = Subtype, y = weight)) +
+  geom_boxplot(alpha = 0.5, width = 0.2) + 
+  geom_violin(aes(Subtype, fill = Subtype),
+              scale = "width", alpha = 0.8) +
+  scale_fill_manual(values = cbcols) +
+  labs(x = "MSI Status", y = "weight") +
+  theme_bw() +
+  theme(axis.text = element_text(size = 16)) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(legend.direction = "horizontal", legend.position = "top") + 
+  stat_compare_means(comparisons = my_comparisons,
+                     label = "p.signif", method = "wilcox.test")
+
+
+
+emptycols <- sapply(that1, function (k) all(k == "[Not Available]"))
+that1 <- that1[!emptycols]
+emptycols <- sapply(that1, function (k) all(k == ""))
+that1 <- that1[!emptycols]
+
+
+View(colnames(that1))
+
+dcast(that2, Subtype ~ history_of_neoadjuvant_treatment)
+
+
+that3 <- droplevels(subset(that2, preoperative_pretreatment_cea_level != "[Not Available]"))
+that3$preoperative_pretreatment_cea_level <- as.numeric(as.character(that3$preoperative_pretreatment_cea_level))
+
+ggplot(that3, aes(x = Subtype, y = preoperative_pretreatment_cea_level)) +
   geom_boxplot(alpha = 0.5, width = 0.2) + 
   geom_violin(aes(Subtype, fill = Subtype),
               scale = "width", alpha = 0.8) +
