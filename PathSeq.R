64a0e103ed968c9ecc3d068630820d222b60cd36
JackLMc <jacklukemcmurray@gmail.com>
2019-04-10 12:31:16 +0100

    Writing script to analyse the pathseq data

--- PathSeq.R
+++ PathSeq.R
@@ -14,25 +14,31 @@ cbcols <- c("MSS-hiCIRC" = "#999999",
 
 # Looking for the files
 thousand.folders <- list.dirs(path = "/Volumes/2018/beggsa-tcgacolorectal/download_rest/bacterial_project/results", full.names = T)
-filelist1 <- sapply(thousand.folders[-1], function(x){
+filelist1 <- sapply(thousand.folders, function(x){
   list.files(x, pattern = ".pathseq.txt$", full.names = T)})
 filelist = unlist(filelist1)
 
+
+
 # Read in files and combine
-lists <- lapply(filelist, read.delim, header = F)
+lists <- lapply(filelist, read.delim, header = T)
 listsDF <- lists
 lists <- listsDF
 
-for (i in names(lists)){
-  p <- gsub("/Volumes/2018/beggsa-tcgacolorectal/download_rest/bacterial_project/results", "", i)
-  colnames(lists[[i]]) <- c("Gene", p)
-}
-
-names(lists) <- gsub("./Data/FPKM/", "", names(lists))
+names(lists) <- filelist
+names(lists) <- gsub("/Volumes/2018/beggsa-tcgacolorectal/download_rest/bacterial_project/results/", "", names(lists))
+names(lists) <- gsub("_gdc_realn.pathseq.txt", "", names(lists))
+names(lists) <- gsub("_hg19_Illumina", "", names(lists))
 
 
+names(lists)
 # Colnames = c(tax_id, taxonomy, type, name,
 # kingdom, score, score_normalized, reads, unambiguous, reference_length)
+GDC_convert <- read.delim("./Data/GDC_large_mapping_TCGA.txt")
+GDC_convert$Patient.ID <- gsub("-", ".", GDC_convert$cases.0.submitter_id)
 
+colnames(GDC_convert)[colnames(GDC_convert) == "cases.0.samples.0.portions.0.analytes.0.aliquots.0.submitter_id"] <- "file.ID"
+head(GDC_convert)
 
+COADREAD_GDC <- droplevels(subset(GDC_convert, cases.0.project.project_id == "TCGA-COAD" | cases.0.project.project_id == "TCGA-READ"))
 
