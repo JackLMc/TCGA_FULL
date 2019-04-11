47da6c0e3bf477c1a40ddaa3725393e6482937a7
JackLMc <jacklukemcmurray@gmail.com>
2019-04-11 10:40:58 +0100

    Scripts to fill in the missing data from the patients

--- PathSeq.R
+++ PathSeq.R
@@ -40,12 +40,12 @@ GDC_convert_n <- droplevels(subset(GDC_convert, Sample.Type == "Blood Derived No
                                      Sample.Type == "Solid Tissue Normal"))
 
 
-lists1 <- lists[names(lists) %in% GDC_convert_n$File.Name]
+lists1 <- lists[names(lists) %in% GDC_convert_t$File.Name]
 lists2 <- lapply(names(lists1), 
                   function(n, x){
                     x[[n]]$File.Name <- n
-                    return (x[[n]])
-                  }, lists1)
+                    return (x[[n]])},
+                 lists1)
 
 
 multi_join <- function(list_of_loaded_data, join_func, ...){
@@ -59,6 +59,7 @@ pathseq <- multi_join(lists2, full_join, by = c("tax_id", "taxonomy", "type", "n
                                                     "reads", "unambiguous", "reference_length", "File.Name"))
 
 pathseq1 <- merge(pathseq, GDC_convert, by = "File.Name") %>% droplevels()
+pathseq1$Patient.ID <- as.factor(pathseq1$Patient.ID)
 pathseq1$kingdom <- as.factor(pathseq1$kingdom)
 pathseq1$name <- as.factor(pathseq1$name)
 pathseq1$taxonomy <- as.factor(pathseq1$taxonomy)
@@ -66,27 +67,57 @@ pathseq1$Patient.ID <- as.factor(pathseq1$Patient.ID)
 pathseq1$type <- as.factor(pathseq1$type)
 
 
-pathseq2 <- pathseq1[, c("Patient.ID", "kingdom", "type", "name", "score",
-                         "score_normalized", "reads", "unambiguous", "reference_length")]
+pathseq2 <- pathseq1[, c("Patient.ID", "name", "score",
+                         "score_normalized", "reads", "kingdom", "type", "unambiguous", "reference_length")]
 
-levels(pathseq2$kingdom)
-head(pathseq2)
+# Fill in the missing data for microbes not found in those samples
+microbe_taxa <- pathseq1[, c("kingdom", "type", "name", "reference_length")]
+microbe_taxa <- microbe_taxa[!duplicated(microbe_taxa), ]
 
-droplevels(subset(pathseq2, type == "no_rank"))
+missing_microbes <- list()
+for(i in levels(pathseq2$Patient.ID)){
+  work <- droplevels(subset(pathseq2, Patient.ID == i))
+  pat_microbes <- as.character(levels(work$name))
+  all_microbes <- as.character(levels(pathseq2$name))
+  see <- all_microbes[!(all_microbes %in% pat_microbes)]
+  missing <- ifelse((see != 0), c(see), "None")
+  missing_microbes[[i]] <- missing
+  }
+
+missing_microbes1 <- lapply(missing_microbes, as.data.frame, stringsAsFactors = F)
+missing_microbes2 <- lapply(missing_microbes1, setNames, c("name"))
+
+missing_microbes3 <- lapply(names(missing_microbes2), 
+                 function(n, x){
+                   x[[n]]$Patient.ID <- n
+                   return (x[[n]])},
+                 missing_microbes2)
+
+missing_microbes3 <- mapply(cbind, missing_microbes3, "score" = 0, SIMPLIFY = F)
+missing_microbes3 <- mapply(cbind, missing_microbes3, "score_normalized" = 0, SIMPLIFY = F)
+missing_microbes3 <- mapply(cbind, missing_microbes3, "reads" = 0, SIMPLIFY = F)
+missing_microbes3 <- mapply(cbind, missing_microbes3, "unambiguous" = 0, SIMPLIFY = F)
 
 
+missing_data <- multi_join(missing_microbes3, full_join, by = c("Patient.ID", "name", "score", "score_normalized",
+                                                "reads", "unambiguous"))
+missing_data <- merge(missing_data, microbe_taxa, by = "name")
+pathseq3 <- rbind(pathseq2, missing_data)
 
-output <- data.frame(stringsAsFactors = F)
-c <- 1
-for(i in levels(pathseq1$Patient.ID)){
-  work <- droplevels(subset(pathseq1, Patient.ID == i))
-  these <- nlevels(work$taxonomy)
-  output[c, "Patient.ID"] <- i
-  output[c, "num_of_tax"] <- these
-  c <- c + 1
-}
+# ## Double-check
+# output <- data.frame(stringsAsFactors = F)
+# c <- 1
+# for(i in levels(pathseq3$Patient.ID)){
+#   work <- droplevels(subset(pathseq3, Patient.ID == i))
+#   these <- nlevels(work$name)
+#   output[c, "Patient.ID"] <- i
+#   output[c, "num_of_tax"] <- these
+#   c <- c + 1
+# }
 
-head(output)
-
-
+# Merge with the pat_sub file
 pat_sub <- read.csv("./Output/Patient_Subtypes.csv")
+pathseq4 <- merge(pathseq3, pat_sub, by = "Patient.ID")
+
+head(pathseq4)
+
