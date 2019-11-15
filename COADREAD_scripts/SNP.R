6716490365afb091cfd3e8e91a64112a005e7412
JackLMc <jacklukemcmurray@gmail.com>
2019-11-15 15:57:03 +0000

    SNP investigation... Need a file to map IDs to old TCGA code

--- COADREAD_scripts/SNP.R
+++ COADREAD_scripts/SNP.R
@@ -8,8 +8,34 @@ SNP <- read.delim("./Data/SNPs/matrix_genotypes.final.txt")
 SNP1 <- column_to_rownames(SNP, var = "snpID") %>% 
   as.matrix() %>%
   t()
-rownames(SNP) <- gsub("^X", "", rownames(SNP))
+rownames(SNP1) <- gsub("^X", "", rownames(SNP1))
 
+
+manifest <- read.delim("./Data/SNPs/manifest.txt")
+
+
+
+mapping <- read.delim("./Data/GDC_large_mapping_TCGA.txt")
+
+manifest[manifest$filename %in% mapping$file_name,]
+
+
+
+mapping$id <- gsub("-", ".", mapping$id)
+
+
+install.packages("rjson")
+library("rjson")
+
+meta <- fromJSON(file = "~/Downloads/metadata.cart.2019-11-15.json")
+head(meta[[2]])
+
+
+
+mapping[mapping$id  %in% rownames(SNP1),]
+
+
+head(rownames(SNP1))
 
 
 
