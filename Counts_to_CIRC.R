38f605440883c53a9a77eaa9e971996d8ad92816
JackLMc <jacklukemcmurray@gmail.com>
2020-01-31 13:17:05 +0000

    Normalisation of Counts

--- Counts_to_CIRC.R
+++ Counts_to_CIRC.R
@@ -53,11 +53,6 @@ library(reshape2)
 
 Counts <- dcast(temp_df1, Gene ~ Patient.ID, sum, value.var = "Count") # This does nothing, but get in correct format.
 
-# library_sizes <- colSums(Counts[!'%in%'(colnames(Counts), "Gene")]) %>% as.data.frame() %>%
-#   rownames_to_column(., var = "Patient.ID")
-# colnames(library_sizes) <- c("Patient.ID", "Library_size")
-# write.csv("./Output/Library_Sizes.csv", x = library_sizes, row.names = F)
-
 Counts_cleaned <- droplevels(Counts[!'%in%'(Counts$Gene, 
                                             c("__alignment_not_unique", "__no_feature",
                                               "__not_aligned", "__too_low_aQual", "__ambiguous")), ])
@@ -70,10 +65,10 @@ library(biomaRt)
 ensembl_DB <- useEnsembl(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
 
 Gene_Map <- getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"),
-      filters = "ensembl_gene_id", values = Counts_cleaned$Gene, mart = ensembl_DB)
+      filters = "ensembl_gene_id", values = Counts_cleaned$Gene, ensembl_DB) # Only finds 56,543 of the 60,483 genes
 
 
-rm(list = setdiff(ls(), c("Gene_Map", "Counts_cleaned"))) # Clean environment
+rm(list = setdiff(ls(), c("Gene_Map", "Counts_cleaned", "ensembl_DB"))) # Clean environment
 save.image("./R_Data/Counts_clean.RData")
 
 load("./R_Data/Counts_clean.RData")
@@ -82,27 +77,89 @@ library(tidyverse)
 library(ggpubr)
 
 colnames(Counts_cleaned)[colnames(Counts_cleaned) == "Gene"] <- "ensembl_gene_id"
-Counts_SYMBOLS <- merge(Counts_cleaned, Gene_Map, by = "ensembl_gene_id") %>% 
-  dplyr:: select(., -ensembl_gene_id) %>% gather(., key = contains("TCGA"), value = "Count")
 
+Counts_merged <- merge(Counts_cleaned, Gene_Map, by = "ensembl_gene_id")  
+Counts_SYMBOLS <- Counts_merged[, !'%in%'(colnames(Counts_merged), "ensembl_gene_id")]
+
+Counts_long <- Counts_SYMBOLS %>% gather(-hgnc_symbol, value = "Count", key = "Patient.ID")
 
 # The below function adds the counts together to account for the many:1 mapping of ensembl_gene_id to hgnc_symbols
 # hgnc_symbols are much easier to work with for my analysis - Neeraj determined all of CIRC in this
 # Big assumption, however, is that I assume the various loci are producing proteins with the same function
-Counts_totalled <- dcast(Counts_SYMBOLS, hgnc_symbol ~ Patient.ID, sum, value.var = "Count")
+Counts_totalled <- dcast(Counts_long, hgnc_symbol ~ Patient.ID, sum, value.var = "Count")
 
 Counts <- Counts_totalled[!is.na(Counts_totalled$hgnc_symbol), ]
-rownames(Counts) <- NULL
+Counts$hgnc_symbol <- as.factor(Counts$hgnc_symbol)
 
-## Should these be used to talk about library sizes?
-# Patient_list <- colnames(FPKMs)[!'%in%'(colnames(FPKMs), "Gene")]
-# write.table(Patient_list, "./Output/Patient_list.txt", row.names = F)
+Counts <- droplevels(subset(Counts, hgnc_symbol != "")) # Remove genes which don't have a hgnc_symbol
+row.names(Counts) <- NULL
+Counts <- column_to_rownames(Counts, var = "hgnc_symbol")
 
+library_sizes <- colSums(Counts[!'%in%'(colnames(Counts), "hgnc_symbol")]) %>% as.data.frame() %>%
+  rownames_to_column(., var = "Patient.ID")
+colnames(library_sizes) <- c("Patient.ID", "Library_size")
+CPM_scaling <- min(library_sizes$Library_size)/1000000 # Calcualte the cpm scaling value based on the smallest library size
+cpm_scale <- 6.5/CPM_scaling # 6.5 count = minimum I believe (this is from edgeR user manual)
 
+cpms <- cpm(Counts)
+average <- rowMeans(cpms)
 
-BiocManager::install("cqn")
+Genes_to_keep <- average[average >= cpm_scale] %>% names() # Only keep the genes which have an average cpm above the scaling value
+# This is how it was done in the GSVA paper
 
-FPKM2[!'%in%'(colnames(FPKM2), c("SYMBOL"))] <- log2(FPKM2[!'%in%'(colnames(FPKM2), c("SYMBOL"))] + 1)
+Counts_above_6.5 <- Counts[rownames(Counts) %in% Genes_to_keep, ]
+
+rm(list = setdiff(ls(), c("Gene_Map", "Counts_above_6.5", "ensembl_DB"))) # Clean environment
+
+# Normalisation
+# BiocManager::install("cqn")
 library(cqn)
+Gene_Map1 <- getBM(attributes = c("hgnc_symbol", "percentage_gene_gc_content", "start_position", "end_position"),
+                   filters = "hgnc_symbol", values = rownames(Counts_above_6.5), ensembl_DB)
+libraries <- colSums(Counts_above_6.5[!'%in%'(colnames(Counts_above_6.5), "hgnc_symbol")])
 
-?cqn
+duplicate_info <- Gene_Map1$hgnc_symbol[duplicated(Gene_Map1$hgnc_symbol)] # These genes have multiple numbers for GC or Start or end positions
+length(duplicate_info)
+
+Gene_Map1$hgnc_symbol <- as.factor(Gene_Map1$hgnc_symbol)
+Gene_Map2 <- data.frame()
+c <- 1
+for(i in levels(Gene_Map1$hgnc_symbol)){
+  print(i)
+  working <- droplevels(subset(Gene_Map1, hgnc_symbol == i))
+  Average_GC <- mean(working$percentage_gene_gc_content)
+  Average_start <- mean(working$start_position)
+  Average_end <- mean(working$end_position)
+  Gene_Map2[c, "hgnc_symbol"] <- i
+  Gene_Map2[c, "Average_percentage_gc_content"] <- Average_GC
+  Gene_Map2[c, "Average_start_position"] <- Average_start
+  Gene_Map2[c, "Average_end_position"] <- Average_end
+  c <- c + 1
+}
+
+Gene_Map2$length <- Gene_Map2$Average_end_position - Gene_Map2$Average_start_position
+row.names(Gene_Map2) <- NULL
+
+Gene_Map3 <- column_to_rownames(Gene_Map2, var = "hgnc_symbol")
+stopifnot(all(rownames(Counts_above_6.5) == rownames(Gene_Map3)))
+stopifnot(colnames(Counts_above_6.5) == names(libraries))
+
+# Normalisation
+cqn_Counts <- cqn(Counts_above_6.5, lengths = Gene_Map3$length, x = Gene_Map3$Average_percentage_gc_content,
+                  sizeFactors = libraries, verbose = T)
+
+
+## Look at systematic effects, n argument refers to the effect, 1 is always the covariate specified by the x argument above (gc content)
+## whereas 2 is lengths
+par(mfrow = c(1, 2))
+cqnplot(cqn_Counts, n = 1, xlab = "GC content", lty = 1, ylim = c(1,7))
+cqnplot(cqn_Counts, n = 2, xlab = "length", lty = 1, ylim = c(1,7))
+
+
+## Normalised expression values
+Counts_cqn <- cqn_Counts$y + cqn_Counts$offset
+
+rm(list = setdiff(ls(), c("Gene_Map3", "Counts_above_6.5", "ensembl_DB", "Counts_cqn", "cqn_Counts", "libraries"))) # Clean environment
+
+
+head(Counts_cqn) # These values are on a log2 scale.
