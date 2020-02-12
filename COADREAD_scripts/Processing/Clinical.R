bf055eb936bfaa9e60c846717f9c7dc3a5dc9cf0
JackLMc <jacklukemcmurray@gmail.com>
2020-02-12 15:44:12 +0000

    Count the number of patients belonging to each MMR group

--- COADREAD_scripts/Processing/Clinical.R
+++ COADREAD_scripts/Processing/Clinical.R
@@ -4,7 +4,7 @@ library(UsefulFunctions)
 library(tidyverse)
 library(ggpubr)
 
-# Collate the patients I want to look for (taken from 1_FPKM.R)
+# Collate the patients I want to look for (taken from Counts_to_CIRC.R)
 Patient_list <- read.delim("./Output/Patient_list.txt")
 Patient_list <- levels(Patient_list$x)
 
@@ -22,7 +22,7 @@ Paper_clinical$Patient.ID <- gsub("-", ".", Paper_clinical$Patient.ID)
 # capp <- tcga_pub_clinical[tcga_pub_clinical$Patient.ID %in% Paper_clinical$Patient.ID,]
 
 ## find the patients in the paper clinical data
-clin_app <- Paper_clinical[Paper_clinical$Patient.ID %in% Patient_list, ] # Finds 380 Patients
+clin_app <- Paper_clinical[Paper_clinical$Patient.ID %in% Patient_list, ] # Finds 332 Patients
 COADREAD <- droplevels(subset(Paper_clinical, Cancer.type == "COAD" | Cancer.type == "READ"))
 
 ## Find the remaining patients
@@ -40,7 +40,8 @@ FD <- full_Data[!duplicated(full_Data), ]
 FD1 <- FD[!is.na(FD$MSI_STATUS),] %>% droplevels()
 FD1$MSI_STATUS <- ifelse((FD1$MSI_STATUS == "MSI-H"), "MSI-H", "MSS") ## Calls MSI-L patients MSS
 
+count(FD1, vars = c("MSI_STATUS"))
 
-write.csv("./Output/Clinical_Data_614.csv", x = FD1, row.names = F)
+write.csv("./Output/Clinical_Data_543.csv", x = FD1, row.names = F)
 
 #### END ####
