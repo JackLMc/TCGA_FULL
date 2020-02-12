c0dec5be35dff49f5376eefa7b3a0512d9d3e8fc
JackLMc <jacklukemcmurray@gmail.com>
2020-02-12 15:44:35 +0000

    Write the patient list out

--- COADREAD_scripts/Processing/Counts_to_CIRC.R
+++ COADREAD_scripts/Processing/Counts_to_CIRC.R
@@ -245,7 +245,7 @@ rm(list = setdiff(ls(), c("Gene_Map3", "ensembl_DB", "Counts_cqn", "cqn_Counts",
 
 
 head(Counts_cqn) # These values are on a log2 scale.
-
+write.table("./Output/Patient_list.txt", x = as.factor(colnames(Counts_cqn)), row.names = F)
 save.image("./R_Data/Counts_clean.RData")
    
 
