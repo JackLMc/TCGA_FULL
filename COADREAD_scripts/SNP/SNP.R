f88b044777df6a4957091fd9ee3455a68998c4f7
JackLMc <jacklukemcmurray@gmail.com>
2020-02-17 11:16:11 +0000

    Association tests

--- COADREAD_scripts/SNP/SNP.R
+++ COADREAD_scripts/SNP/SNP.R
@@ -2,14 +2,17 @@
 library(tidyverse)
 library(UsefulFunctions)
 
-## From Boris
 # Looking at the SNPs
-MSS_hiCIRC <- read.delim("./Data/SNPs/Boris/association.annotated.txt") # A = MSS, B = MSS_hiCIRC
-Autosomes <- MSS_hiCIRC[!('%in%'(MSS_hiCIRC$Chromosome, c("X", "Y", "MT"))),] %>% droplevels()
+MSS_hiCIRC <- read.delim("./Output/associations.annotated_13_02.txt") # A = MSS, B = MSS_hiCIRC
+
+# Split into Autosomes and Mitochondria 
+Autosomes <- MSS_hiCIRC[!('%in%'(MSS_hiCIRC$Chromosome, c("X", "Y", "MT"))),] %>% droplevels() # Look at the allele test, phred score around 60 = 10E-6 Pval
 # write.csv(x = Autosomes, file = "./Output/Boris_Autosome_Associations_MSS_MSShiCIRC.csv", row.names = F)
 
-Mito <- MSS_hiCIRC[('%in%'(MSS_hiCIRC$Chromosome, c("MT"))),] %>% droplevels()
+Mito <- MSS_hiCIRC[('%in%'(MSS_hiCIRC$Chromosome, c("MT"))),] %>% droplevels() # Look at the genotype test, phred score around 25?
 # write.csv(x = Mito, file = "./Output/Boris_Mito_Associations_MSS_MSShiCIRC.csv", row.names = F)
+
+
 
 
 ordered_Mito <- Mito[order(Mito$genotype_rest_phred, decreasing = T),]
