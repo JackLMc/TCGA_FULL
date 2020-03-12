64ff29e9b594147322928543cd6104015cb3acea
JackLMc <jacklukemcmurray@gmail.com>
2020-03-12 13:25:38 +0000

    SNPs have no significance

--- COADREAD_scripts/SNP/SNP.R
+++ COADREAD_scripts/SNP/SNP.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(UsefulFunctions)
 
 # Looking at the SNPs
-MSS_hiCIRC <- read.delim("./Output/associations.annotated_13_02.txt") # A = MSS, B = MSS_hiCIRC
+MSS_hiCIRC <- read.delim("./Output/associations.annotated_12_03.txt") # A = MSS, B = MSS_hiCIRC
 
 # Split into Autosomes and Mitochondria 
 Autosomes <- MSS_hiCIRC[!('%in%'(MSS_hiCIRC$Chromosome, c("X", "Y", "MT"))),] %>% droplevels() # Look at the allele test, phred score around 60 = 10E-6 Pval
@@ -21,7 +21,17 @@ head(ordered_Mito)
 ordered_Auto <- Autosomes[order(Autosomes$allele_test_phred, decreasing = T),]
 head(ordered_Auto)
 
-head(ordered_Mito)
+
+# No significance
+shuffled <- read.delim("./Output/all.txt")
+head(shuffled)
+
+above_threshold <- droplevels(subset(shuffled, allele_test_phred > 21))
+nrow(above_threshold)
+
+
+above_thresh <- droplevels(subset(ordered_Auto, allele_test_phred > 21))
+nrow(above_thresh)
 
 
 #### END ####
