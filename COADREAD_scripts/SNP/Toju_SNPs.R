140cd5c0649e25e665fc54400f2295d57bbc6db6
JackLMc <jacklukemcmurray@gmail.com>
2020-03-12 10:02:07 +0000

    Only 128 of them are found...

--- COADREAD_scripts/SNP/Toju_SNPs.R
+++ COADREAD_scripts/SNP/Toju_SNPs.R
@@ -1,5 +1,14 @@
 # A script to reduce the matrix genotypes file to only those SNPs that are in Toju's analysis
+library(tidyverse)
+
 ## Read in required data
+SNPs_to_read <- read.csv("./Data/SNPs/Toju/SNP_Toju.csv")$SNP # these are rs numbers... Need to do this on the final matrix...
+get <- read.delim("./Data/SNPs/14_02/annotation/SNP_annotations.hg19.txt")[, c("Probe_Set_ID", "dbSNP_RS_ID")]
+
+these_probes <- get[get$dbSNP_RS_ID %in% SNPs_to_read, ]
+
+
+## Subset input dataframe
 SNP <- read.delim("./Data/SNPs/matrix_genotypes.final.txt")
 
 SNP1 <- column_to_rownames(SNP, var = "snpID") %>%
@@ -7,12 +16,9 @@ SNP1 <- column_to_rownames(SNP, var = "snpID") %>%
   t()
 rownames(SNP1) <- gsub("^X", "", rownames(SNP1))
 
+SNP2 <- SNP1[, colnames(SNP1) %in% these_probes$Probe_Set_ID]
 
 
-SNPs_to_read <- read.csv("./Data/SNPs/Toju/SNP_Toju.csv")$SNP # these are rs numbers... Need to do this on the final matrix...
-
-
-SNP2 <- SNP1[, colnames(SNP1) %in% SNPs_to_read]
-
-
+# Write out new input dataframe
+write.table("./Output/SNP/matrix_genotypes.subset.txt", x = SNP2, row.names = F, quote = F, sep = "\t")
 
