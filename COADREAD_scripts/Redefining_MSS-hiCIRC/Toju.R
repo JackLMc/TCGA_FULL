2235c5e2d3a5d60f533f625da8d3e43fea9ecc1d
JackLMc <jacklukemcmurray@gmail.com>
2020-06-24 15:49:28 +0100

    Not needed script

--- COADREAD_scripts/Redefining_MSS-hiCIRC/Toju.R
+++
@@ -1,6 +0,0 @@
-# A R Script to normalise toju's RNA sequencing
-Toju <- read.delim("./Data/Toju/Combined_Immunoscore_Quantify_to_annotation_model_(Partek_E_M)_Gene_counts.txt")
-
-
-
-head(Toju)[, 1:10]
