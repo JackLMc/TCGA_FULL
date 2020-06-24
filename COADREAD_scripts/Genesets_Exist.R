9ff9f062cb42fa813898c49b39b77f550ed92f30
JackLMc <jacklukemcmurray@gmail.com>
2020-06-24 16:57:13 +0100

    Add code to check for the genes and the replacements

--- COADREAD_scripts/Genesets_Exist.R
+++ COADREAD_scripts/Genesets_Exist.R
@@ -3,20 +3,22 @@ library(UsefulFunctions)
 library(tidyverse)
 library(ggpubr)
 
-my_comparisons <- list(c("MSS-hiCIRC", "MSI-H"),
-                       c("MSS-hiCIRC", "MSS"),
-                       c("MSI-H", "MSS"))
-cbcols <- c("MSS-hiCIRC" = "#999999",
-            "MSI-H" = "#56B4E9",
-            "MSS" = "#009E73",
-            "MSI-L" = "#E69F00")
-
-
-
-CTGenesets <- read.csv("./Exploratory_Data/Genesets/Investigative_Genesets.csv", stringsAsFactors = T)
-SigGen <- read.csv("./Exploratory_Data/Genesets/Signature_Genesets.csv")
-
-
+# Load data
 load("./R_Data/Counts_clean.RData")
 
-pat_sub <- read.csv("./Output/Patient_Subtypes_09_03.csv")
+# The Genesets
+## Cell type Genesets
+CTGenesets <- read.csv("./Exploratory_Data/Genesets/Investigative_Genesets.csv", stringsAsFactors = T)
+
+CTGenesets[!'%in%'(CTGenesets$HUGO.symbols, rownames(Counts_cqn)), ]
+rownames(Counts_cqn)[grep("CD4", rownames(Counts_cqn))]
+
+
+# Signature Genesets
+SigGen <- read.csv("./Exploratory_Data/Genesets/Signature_Genesets.csv", stringsAsFactors = T)
+
+SigGen[!'%in%'(SigGen$HUGO.symbols, rownames(Counts_cqn)), ]
+rownames(Counts_cqn)[grep("CD4", rownames(Counts_cqn))]
+
+
+
